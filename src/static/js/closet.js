4bf626108a1c19b87eda9612a64050c691b013e6
Ismaelleon <Ismaelleon@protonmail.com>
2024-07-14 02:31:23 -0300

    fix: add onclick events directly on html

--- src/static/js/closet.js
+++ src/static/js/closet.js
@@ -1,10 +1,3 @@
-// Toggle outfit menu
-const outfitsEls = document.querySelectorAll('#clothing-list > section > header > button');
-
 function toggleMenu (event) {
 	event.currentTarget.querySelector('ul').classList.toggle('hidden');
-}
-
-for (let outfitEl of outfitsEls) {
-	outfitEl.addEventListener('click', toggleMenu)
 }
