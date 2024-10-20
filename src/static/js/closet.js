115140002f9a725b769015bfad176c824ca0772f
Ismaelleon <Ismaelleon@protonmail.com>
2024-10-20 13:20:32 -0300

    styles: make filters button gray when filter menu is open

--- src/static/js/closet.js
+++ src/static/js/closet.js
@@ -2,6 +2,7 @@ function toggleMenu(event) {
 	event.currentTarget.querySelector("ul").classList.toggle("hidden");
 }
 
-function toggleFiltersMenu() {
+function toggleFiltersMenu(event) {
 	document.getElementById("filters-menu").classList.toggle("hidden");
+	event.currentTarget.classList.toggle("bg-zinc-200");
 }
