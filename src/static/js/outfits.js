c3a488a058d2da7e16b6e4ba5c57e659e4c47c71
Ismaelleon <Ismaelleon@protonmail.com>
2025-01-06 22:31:14 -0300

    feat: add deletion confirmation

--- src/static/js/outfits.js
+++ src/static/js/outfits.js
@@ -6,3 +6,23 @@ function toggleFiltersMenu(event) {
 	document.getElementById("filters-menu").classList.toggle("hidden");
 	event.currentTarget.classList.toggle("bg-zinc-200 dark:bg-zinc-800");
 }
+
+function hideDeletionModal() {
+	const deletionModal = document.getElementById("deletion-modal");
+	deletionModal.classList.add("hidden");
+}
+
+function showDeletionModal(event) {
+	const deletionModal = document.getElementById("deletion-modal");
+	const outfitEl = event.target.closest("section");
+	const deleteButton = deletionModal.firstElementChild.children[1];
+
+	// Show deletion modal
+	deletionModal.classList.remove("hidden");
+
+	// Set the attribute to use the DELETE method, with the outfit id
+	deleteButton.setAttribute("hx-delete", `/outfits/delete/${outfitEl.id}`);
+
+	// Make htmx process the button
+	htmx.process(deleteButton);
+}
