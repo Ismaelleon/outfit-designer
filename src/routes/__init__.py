48d025bb63a633330e4de31d5aa246b59ed24da5
Ismaelleon <Ismaelleon@protonmail.com>
2025-01-08 04:24:03 -0300

    feat: add user deletion to settings page

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -438,7 +438,7 @@ def setup_router (app, mongo):
             if user == None:
                 return handle_invalid_user_session()
 
-            data = dark_mode({ "user": user, "activated": user["activation"]["activated"] }, request.cookies)
+            data = dark_mode({ "user": user, "activated": user["activation"]["activated"], "modal-hidden": True }, request.cookies)
             return render_template("settings.html", data=data)
 
         return redirect("/")
@@ -559,6 +559,33 @@ def setup_router (app, mongo):
         res = make_response({"message": "OK"}, 200)
         res.headers["HX-Redirect"] = "/outfits"
         return res 
+
+    @app.route("/account/delete", methods=["DELETE"])
+    def delete_account():
+        if session.get("id"):
+            # Get user document 
+            user_id = session.get("id")
+            user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+
+            # Get password form value
+            password = request.form["password"]
+
+            # Check if passwords match
+            if bcrypt.checkpw(password.encode("ascii"), user["password"].encode("ascii")):
+                # If password is valid, delete the account
+                mongo.db.users.delete_one(user)
+
+                # Remove the session
+                session.pop("id", None)
+
+                # Redirect user to /
+                res = make_response({ "message": "OK" }, 200)
+                res.headers["HX-Redirect"] = "/"
+                return res
+
+            # If passwords do not match, return delete-account component
+            data = dark_mode({"error": True, "modal-hidden": False}, request.cookies)
+            return render_template("components/delete-account.html", data=data)
 
     @app.route("/log-out", methods=["POST"])
     def log_out ():
