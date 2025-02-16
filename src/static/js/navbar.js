076e5892d2182056f5b4dea5f2ec37c3fdcfa90b
Ismaelleon <Ismaelleon@protonmail.com>
2025-02-16 05:32:49 -0300

    refactor: format files

--- src/static/js/navbar.js
+++ src/static/js/navbar.js
@@ -1,7 +1,11 @@
-function toggleNavbarMenu () {
-	document.getElementById('menu').classList.toggle('hidden');
-	document.getElementById('menu').classList.toggle('flex');
+function toggleNavbarMenu() {
+	document.getElementById("menu").classList.toggle("hidden");
+	document.getElementById("menu").classList.toggle("flex");
 }
 
-document.getElementById('menu-open').addEventListener('click', toggleNavbarMenu);
-document.getElementById('menu-close').addEventListener('click', toggleNavbarMenu);
+document
+	.getElementById("menu-open")
+	.addEventListener("click", toggleNavbarMenu);
+document
+	.getElementById("menu-close")
+	.addEventListener("click", toggleNavbarMenu);
