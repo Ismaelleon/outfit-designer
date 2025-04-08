f3854224bf89b4e77c817bbe5c6596d31b30834b
Ismaelleon <Ismaelleon@protonmail.com>
2025-04-08 01:38:21 -0300

    fix: use bash as shell and start gunicorn correctly

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
-# Get into the app directory
-cd /root/outfit-designer/src/
-
-# Start virtual environment
-source env/bin/activate
-
-# Install dependencies from requirements.txt
-./env/bin/python3 -m pip install -r requirements.txt
-
-# Start gunicorn wsgi
-/usr/bin/gunicorn --bind 0.0.0.0:5000 wsgi:app
+#!/bin/bash 
+ 
+# Get into the app directory 
+cd /root/outfit-designer/ && 
+ 
+# Start virtual environment 
+source ./env/bin/activate && 
+ 
+# Install dependencies from requirements.txt 
+./env/bin/python3 -m pip install -r requirements.txt && 
+ 
+# Get into src directory 
+cd ./src/ 
+ 
+# Start gunicorn wsgi 
+../env/bin/python3 -m gunicorn --bind 0.0.0.0:5000 wsgi:app
