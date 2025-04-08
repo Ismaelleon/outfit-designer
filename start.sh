6ab00f2fdda1073aca08c18be2b0772bfd247c06
Ismaelleon <Ismaelleon@protonmail.com>
2025-04-08 01:45:33 -0300

    cd: add daemon flag to gunicorn

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@
 cd /root/outfit-designer/ && 
  
 # Start virtual environment 
-source ./env/bin/activate && 
+. ./env/bin/activate && 
  
 # Install dependencies from requirements.txt 
 ./env/bin/python3 -m pip install -r requirements.txt && 
  
 # Get into src directory 
-cd ./src/ 
+cd ./src/ &&
  
 # Start gunicorn wsgi 
-../env/bin/python3 -m gunicorn --bind 0.0.0.0:5000 wsgi:app
+../env/bin/python3 -m gunicorn --bind 0.0.0.0:5000 wsgi:app --daemon
