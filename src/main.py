8e4193168c8f5eb66c1970456463011c2279570c
Ismaelleon <Ismaelleon@protonmail.com>
2025-05-24 12:43:39 -0300

    refactor: create function for app setup

--- src/main.py
+++ src/main.py
@@ -8,35 +8,34 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Init flask app 
-app = Flask(__name__, template_folder="views")
+def create_app():
+    # Init flask app 
+    app = Flask(__name__, template_folder="views")
 
-# Setup cloudinary
-app.config["CLOUDINARY"] = cloudinary.config(
-    cloud_name=os.environ["CLOUDINARY_CLOUD_NAME"],
-    api_key=os.environ["CLOUDINARY_API_KEY"],
-    api_secret=os.environ["CLOUDINARY_API_SECRET"],
-    secure=True
-)
+    # Setup cloudinary
+    app.config["CLOUDINARY"] = cloudinary.config(
+        cloud_name=os.environ["CLOUDINARY_CLOUD_NAME"],
+        api_key=os.environ["CLOUDINARY_API_KEY"],
+        api_secret=os.environ["CLOUDINARY_API_SECRET"],
+        secure=True
+    )
 
-# Setup mongo client
-app.config["MONGO_URI"] = os.environ["MONGO_URI"]
-mongo = PyMongo(app)
+    # Setup mongo client
+    app.config["MONGO_URI"] = os.environ["MONGO_URI"]
+    mongo = PyMongo(app)
 
-# Setup uploads folder
-app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), 'static/images')
+    # Setup uploads folder
+    app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), 'static/images')
 
-# Setup server sessions
-app.config["SESSION_PERMANENT"] = True 
-app.config["SESSION_TYPE"] = "mongodb" 
-app.config["SESSION_MONGODB"] = mongo.cx
-app.config["SESSION_MONGODB_DB"] = os.environ["MONGO_DB"] 
-app.config["SESSION_MONGODB_COLLECT"] = "sessions" 
-Session(app)
+    # Setup server sessions
+    app.config["SESSION_PERMANENT"] = True 
+    app.config["SESSION_TYPE"] = "mongodb" 
+    app.config["SESSION_MONGODB"] = mongo.cx
+    app.config["SESSION_MONGODB_DB"] = os.environ["MONGO_DB"] 
+    app.config["SESSION_MONGODB_COLLECT"] = "sessions" 
+    Session(app)
 
-# Setup routes
-setup_router(app, mongo)
+    # Setup routes
+    setup_router(app, mongo)
 
-# Run application
-if __name__ == "__main__":
-    app.run(debug=os.environ["DEBUG"], host="0.0.0.0")
+    return app
