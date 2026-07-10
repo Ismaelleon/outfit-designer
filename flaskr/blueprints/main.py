af6f9502212d63baac19a5ab224ef75b4a923182
Ismaelleon <Ismaelleon@protonmail.com>
2026-07-10 00:48:33 -0300

    feat: add not found page

--- flaskr/blueprints/main.py
+++ flaskr/blueprints/main.py
@@ -1,18 +1,32 @@
-from flask import render_template, request, send_from_directory, session, redirect, Blueprint
+from flask import (
+    render_template,
+    request,
+    send_from_directory,
+    session,
+    redirect,
+    Blueprint,
+)
 from flaskr.helpers import dark_mode
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
+
 # Static files
 @bp.route("/static/<path:file_path>")
-def static_files (file_path):
+def static_files(file_path):
     return send_from_directory("static", file_path)
 
+
 @bp.route("/")
-def index ():
-    # If user logged in, redirect to outfits page 
+def index():
+    # If user logged in, redirect to outfits page
     if session.get("id"):
         return redirect("/outfits")
 
     data = dark_mode({}, request.cookies)
     return render_template("index.html", data={})
+
+
+@bp.app_errorhandler(404)
+def not_found(e):
+    return render_template("not-found.html"), 404
